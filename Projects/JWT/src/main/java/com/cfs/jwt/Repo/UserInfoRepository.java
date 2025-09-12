7b097cf308ce05ce7fbb17b601874d7553aee368
Aman Gautam <amangautam2128@gmail.com>
2025-09-13 00:05:02 +0530

    JWT

---
+++ Projects/JWT/src/main/java/com/cfs/jwt/Repo/UserInfoRepository.java
@@ -0,0 +1,15 @@
+package com.cfs.jwt.Repo;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo , Integer> {
+
+    Optional <UserInfo> findByEmail(String email);
+
+
+}
--- Projects/JWT/src/main/java/com/cfs/jwt/config/SecurityConfig.java
+++ Projects/JWT/src/main/java/com/cfs/jwt/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.cfs.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -65,7 +72,7 @@ public class SecurityConfig {
         return provider;
     }
 
-    Bean
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
