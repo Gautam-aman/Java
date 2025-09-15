9e8a0ea1c73e436b1f8b731e808c35af752462f6
Aman Gautam <amangautam2128@gmail.com>
2025-09-15 22:32:18 +0530

    Created API Gateway

---
+++ APIGateway/src/test/java/com/cfs/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
