e56a982327046af90a4ef216a26d0dbec3dcb333
Aman Gautam <amangautam2128@gmail.com>
2025-10-09 21:04:12 +0530

    Unit test

--- Testing/src/test/java/com/cfs/testing/controller/StudentControllerTest.java
+++ Testing/src/test/java/com/cfs/testing/controller/StudentControllerTest.java
@@ -1,4 +1,53 @@
 package com.cfs.testing.controller;
 
+
+import com.cfs.testing.entity.Student;
+import com.cfs.testing.repo.StudentRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper ;
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @BeforeEach
+    void cleanUp(){
+        studentRepo.deleteAll();
+    }
+
+    void shouldCreateStudent() throws Exception{
+        Student student = new Student(null , "aman" , "aman" , "23");
+
+        mockMvc.perform(post("/api/student")
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .content(mapper.writeValueAsString(student)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("aman"));
+
+
+    }
+
 }
