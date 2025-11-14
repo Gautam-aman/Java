434787224d578b7d7144f6c5adbf66be53a0d1e8
Aman Gautam <amangautam2128@gmail.com>
2025-11-14 17:38:55 +0530

    Thread safe

--- DesignPattern/src/main/java/pattern/singleton/Singleton.java
+++ DesignPattern/src/main/java/pattern/singleton/Singleton.java
@@ -1,24 +1,21 @@
 package pattern.singleton;
 
 
-class Single{
+class Single {
 
-    static Single instance = null;
-    private Single(){
+    private Single() {
         System.out.println("Object Created");
     }
 
-    static Single getInstance(){
-        if (instance == null) { // 1st check (no locking)
-            synchronized (Single.class) {
-                if (instance == null) { // 2nd check (with locking)
-                    instance = new Single();
-                }
-            }
-        }
-        return instance;
+    private static class SingletonHelper {
+        private static final Single INSTANCE = new Single();
+    }
+
+    public static Single getInstance() {
+        return SingletonHelper.INSTANCE;
     }
 }
+
 
 public class Singleton {
     public static void main(String[] args) {
