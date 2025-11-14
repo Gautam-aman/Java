99d38aa37ab68d5ec95a416d650bb3532c85807a
Aman Gautam <amangautam2128@gmail.com>
2025-11-14 17:33:55 +0530

    Completed Singleton

--- DesignPattern/src/main/java/pattern/singleton/Singleton.java
+++ DesignPattern/src/main/java/pattern/singleton/Singleton.java
@@ -1,5 +1,25 @@
 package pattern.singleton;
 
-public class Singleton {
 
+class Single{
+
+    static Single Instance = null;
+    private Single(){
+        System.out.println("Object Created");
+    }
+
+    static Single getInstance(){
+        if (Instance==null){
+            Instance=new Single();
+        }
+        return Instance;
+    }
+}
+
+public class Singleton {
+    public static void main(String[] args) {
+        Single single=Single.getInstance();
+        Single single1=Single.getInstance();
+        System.out.println(single==single1);
+    }
 }
