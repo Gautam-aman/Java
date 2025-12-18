674e8f88df13252b29dc1b0ed5ce0bc164201bd7
Aman Gautam <amangautam2128@gmail.com>
2025-12-18 16:21:10 +0530

    builder completed

--- DesignPattern/src/main/java/pattern/Builder/Main.java
+++ DesignPattern/src/main/java/pattern/Builder/Main.java
@@ -1,4 +1,63 @@
 package pattern.Builder;
 
+// Builder Design pattern
+
+import com.cfs.designpattern.Builder;
+
+class User{
+    private String name;
+    private int age;
+    private int height;
+
+    private User(Builder builder){
+        this.name = builder.name;
+        this.age  = builder.age;
+        this.height = builder.height;
+    }
+
+    public void execute() {
+        System.out.println("User's name is " + name);
+        System.out.println("User's age is " + age);
+        System.out.println("User's height is " + height);
+    }
+
+
+    public static class Builder{
+        private String name;
+        private int age;
+        private int height;
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder setAge(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Builder setHeight(int height){
+            this.height = height;
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+
+        User u = new User.Builder()
+                .setName("Aman")
+                .setAge(21)
+                .build();
+        u.execute();
+        System.out.println("User Build SuccessFully");
+
+
+    }
 }
